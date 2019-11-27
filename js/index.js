93aeed18ef1c08aacf474676667670e07d769324
bdarties <gihtub@darties.fr>
2019-11-27 10:14:19 +0100

    premiere MAJ, test

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ const config = {
   width: 800,
   height: 600,
   parent: "game-container",
-  backgroundColor: "#1d212d",
+  backgroundColor: "#Ad222d",
   pixelArt: true,
   scene: {
     preload: preload,
@@ -27,13 +27,22 @@ let shiftKey;
 let groundLayer;
 
 function preload() {
-  this.load.image("tiles", "../assets/tilesets/0x72-industrial-tileset-32px-extruded.png");
-  this.load.tilemapTiledJSON("map", "../assets/tilemaps/platformer-simple.json");
+  this.load.image(
+    "tiles",
+    "../assets/tilesets/0x72-industrial-tileset-32px-extruded.png"
+  );
+  this.load.tilemapTiledJSON(
+    "map",
+    "../assets/tilemaps/platformer-simple.json"
+  );
 }
 
 function create() {
   const map = this.make.tilemap({ key: "map" });
-  const tiles = map.addTilesetImage("0x72-industrial-tileset-32px-extruded", "tiles");
+  const tiles = map.addTilesetImage(
+    "0x72-industrial-tileset-32px-extruded",
+    "tiles"
+  );
 
   // Same setup as static layers
   map.createDynamicLayer("Background", tiles);
@@ -66,12 +75,17 @@ function create() {
 
   // Help text that has a "fixed" position on the screen
   this.add
-    .text(16, 16, "Arrow keys to scroll\nLeft-click to draw tiles\nShift + left-click to erase", {
-      font: "18px monospace",
-      fill: "#000000",
-      padding: { x: 20, y: 10 },
-      backgroundColor: "#ffffff"
-    })
+    .text(
+      16,
+      16,
+      "Arrow keys to scroll\nLeft-click to draw tiles\nShift + left-click to erase",
+      {
+        font: "18px monospace",
+        fill: "#000000",
+        padding: { x: 20, y: 10 },
+        backgroundColor: "#ffffff"
+      }
+    )
     .setScrollFactor(0);
 }
 
@@ -79,12 +93,17 @@ function update(time, delta) {
   controls.update(delta);
 
   // Convert the mouse position to world position within the camera
-  const worldPoint = this.input.activePointer.positionToCamera(this.cameras.main);
+  const worldPoint = this.input.activePointer.positionToCamera(
+    this.cameras.main
+  );
 
   // Place the marker in world space, but snap it to the tile grid. If we convert world -> tile and
   // then tile -> world, we end up with the position of the tile under the pointer
   const pointerTileXY = groundLayer.worldToTileXY(worldPoint.x, worldPoint.y);
-  const snappedWorldPoint = groundLayer.tileToWorldXY(pointerTileXY.x, pointerTileXY.y);
+  const snappedWorldPoint = groundLayer.tileToWorldXY(
+    pointerTileXY.x,
+    pointerTileXY.y
+  );
   marker.setPosition(snappedWorldPoint.x, snappedWorldPoint.y);
 
   // Draw or erase tiles (only within the groundLayer)
